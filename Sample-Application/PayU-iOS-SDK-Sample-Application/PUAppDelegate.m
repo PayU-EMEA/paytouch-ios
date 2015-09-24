b7be1a626d467a736d2395ed5597e3b27228df0e
Maciej Jastrzebski <mjastrzebski@objectivity.co.uk>
2015-09-24 16:46:14 +0200

    PayTouch iOS SDK v1.4.1

--- Sample-Application/PayU-iOS-SDK-Sample-Application/PUAppDelegate.m
+++ Sample-Application/PayU-iOS-SDK-Sample-Application/PUAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "PUAppDelegate.h"
+#import "PUSDK.h"
 #import "PUMainViewController.h"
 #import "PUPaymentViewController.h"
 #import "AFOAuth2Client.h"
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"PayU SDK: %@", [PUSDK libraryVersion]);
+    
     return YES;
 }
 
